72b5d91b7a2d9eeda14800873b43cde9aaa9fcac
Yingting Huang <yhuan300@ucsc.edu>
2024-02-09 21:38:45 -0800

    changes

---
+++ CP-Scrolling-States-master/src/main.js
@@ -0,0 +1,23 @@
+// Code Practice: Scrolling States
+// Name: Yingting Huang
+// Date: 2/9/23
+
+'use strict'
+
+const config = {
+    parent: 'phaser-game',  // for info text
+    type: Phaser.WEBGL,     // for tinting
+    width: 400,
+    height: 300,
+    pixelArt: true,
+    zoom: 2,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [ Load, Play ]
+}
+
+const game = new Phaser.Game(config)
